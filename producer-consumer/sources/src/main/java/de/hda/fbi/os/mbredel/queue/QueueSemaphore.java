69f631aa313e1b7afc8b59269e5f25b808c10acd
Michael Bredel <michael.bredel@h-da.de>
2019-12-12 10:58:43 +0100

    Added a license header and some documentation.

--- producer-consumer/sources/src/main/java/de/hda/fbi/os/mbredel/queue/QueueSemaphore.java
+++ producer-consumer/sources/src/main/java/de/hda/fbi/os/mbredel/queue/QueueSemaphore.java
@@ -1,6 +1,29 @@
+/*
+ Copyright (c) 2019, Michael von Rüden, H-DA
+ ALL RIGHTS RESERVED.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ Neither the name of the H-DA and Michael Bredel
+ nor the names of its contributors may be used to endorse or promote
+ products derived from this software without specific prior written
+ permission.
+ */
 package de.hda.fbi.os.mbredel.queue;
 
+import de.hda.fbi.os.mbredel.Consumer;
 import de.hda.fbi.os.mbredel.Good;
+import de.hda.fbi.os.mbredel.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +33,14 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * The channel for {@link Producer}-{@link Consumer} exchange.
+ * Internally it uses Semaphores to block additional
+ * put-calls when the queue is full and additional take-calls
+ * when the queue is empty, respectively.
+ *
+ * @author Michael von Rüden
+ */
 public class QueueSemaphore implements IQueue {
     /** The logger. */
     private static final Logger LOGGER = LoggerFactory.getLogger(QueueSemaphore.class);
